29dcdf8e7f94bc1028f3eac11fabfae8ced9df29
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-04-01 10:25:09 +0900

    Initial Commit

---
+++ UISwipeGestureRecognizerSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISwipeGestureRecognizerSample
+//
+//  Created by satoshi.marumoto on 2020/04/01.
+//  Copyright © 2020 satoshi.marumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
